b0962715acf88317a404e32ee91841df593baee3
jinke <jinke5245@163.com>
2024-10-27 13:47:40 +0000

    chore: add script for building and serving book via browser

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cargo install evcxr_jupyter mdbook rustlings
+cargo install mdbook --locked --version 0.4.37
+cargo install rustlings
+cargo install evcxr_jupyter
+
 evcxr_jupyter --install
